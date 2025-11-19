455c1a3dea0a2fe02ed72ac3e2df465c59a7186a
rhythmcreative <rhythmcreative@users.noreply.github.com>
2025-11-19 19:01:22 +0100

    Fix baseUrl configuration and deployment workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
